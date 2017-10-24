47cbe298a1f72a1af03c3e47c7058b1f6099db4f
hefest666 <farvfire@gmail.com>
2017-10-24 14:20:44 +0300

    Created first CS project

---
+++ CSharpHW/Lection_1/HelloViktor/HelloViktor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloViktor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Viktor";
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(myName);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
