ba7b36e29bddfbe358437fb598a458ee72bf3d3c
hefest666 <farvfire@gmail.com>
2017-10-27 13:18:22 +0300

    Перевернул треугольник

--- CSharpHW/Lection_1/HelloViktor/HelloViktor/Program.cs
+++ CSharpHW/Lection_1/HelloViktor/HelloViktor/Program.cs
@@ -10,18 +10,15 @@ namespace HelloViktor
     {
         static void Main(string[] args)
         {
-            string symb = string.Empty;
-
-            Console.ForegroundColor = ConsoleColor.Cyan;
-
-            for(int i = 0; i <= 10; i++)
-            {
-                
-                Console.Write(symb+="*");
-                Console.WriteLine();
-                
-            }
+            string symb = "***************";
             
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            
+            for(int i = 0; i <= symb.Length; i++)
+            {                
+                Console.WriteLine(symb.Remove(symb.Length - i, i));                     
+            }
+                        
             Console.ResetColor();
             Console.ReadKey();
         }
