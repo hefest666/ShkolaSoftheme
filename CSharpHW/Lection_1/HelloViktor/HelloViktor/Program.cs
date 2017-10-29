77e91f08b04df75b7758162c61333e28b4aa5f19
hefest666 <farvfire@gmail.com>
2017-10-29 21:57:35 +0200

    Выполнил ДЗ из 2 лекции

--- CSharpHW/Lection_1/HelloViktor/HelloViktor/Program.cs
+++ CSharpHW/Lection_1/HelloViktor/HelloViktor/Program.cs
@@ -9,17 +9,24 @@ namespace HelloViktor
     class Program
     {
         static void Main(string[] args)
-        {
-            string symb = "***************";
-            
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            
-            for(int i = 0; i <= symb.Length; i++)
-            {                
-                Console.WriteLine(symb.Remove(symb.Length - i, i));                     
-            }
-                        
-            Console.ResetColor();
+        {          
+            Console.WriteLine("int MAX value: " + int.MaxValue);
+            Console.WriteLine("int MIN value: " + int.MinValue + "\n");
+
+            Console.WriteLine("double MAX value: " + double.MaxValue);
+            Console.WriteLine("double MIN value: " + double.MinValue + "\n");
+
+            Console.WriteLine("float MAX value: " + float.MaxValue);
+            Console.WriteLine("float MIN value: " + float.MinValue + "\n");
+
+            Console.WriteLine("short MAX value: " + short.MaxValue);
+            Console.WriteLine("short MIN value: " + short.MinValue + "\n");
+
+            Console.WriteLine("byte MAX value: " + byte.MaxValue);
+            Console.WriteLine("byte MIN value: " + byte.MinValue + "\n");
+
+            Console.WriteLine("long MAX value: " + long.MaxValue);
+            Console.WriteLine("long MIN value: " + long.MinValue + "\n");
             Console.ReadKey();
         }
     }
