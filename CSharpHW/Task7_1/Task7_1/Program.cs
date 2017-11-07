10886eb591b83e49c342b1f3bbda4fe7ae9ce7ae
hefest666 <farvfire@gmail.com>
2017-11-07 23:57:04 +0200

    Task7 Done!

---
+++ CSharpHW/Task7_1/Task7_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var daewo = new Car();
+            daewo.Model = "Daewo Lanos";
+            daewo.Color = "Yellow";
+            daewo.Year = 2008;
+
+            Console.WriteLine("\t\tTuning atelier!");
+            Console.WriteLine("\tBefore tuning:");
+            Console.WriteLine("Car model: {0}", daewo.Model);
+            Console.WriteLine("Car color: {0}", daewo.Color);
+            Console.WriteLine("Car year: {0}", daewo.Year);
+
+            TuningAtelier.TuneCar(daewo);
+            
+            Console.WriteLine("\tAfter tuning:");
+            Console.WriteLine("Car model: {0}", daewo.Model);
+            Console.WriteLine("Car color: {0}", daewo.Color);
+            Console.WriteLine("Car year: {0}", daewo.Year);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpHW/Task7_1/Task7_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alex = new Human();
+            var simon = new Human("14.01.1991", "Simon", "Pashkevich", 26);
+            var simonClone = new Human("14.01.1991", "Simon", "Pashkevich", 26);
+
+            Console.WriteLine("============================");
+            alex.HumanGetInfo();
+            Console.WriteLine("============================");
+            simon.HumanGetInfo();
+            Console.WriteLine("============================");
+            simonClone.HumanGetInfo();
+            Console.WriteLine("============================");
+
+            alex.Compare(simon);
+            simon.Compare(simonClone);
+
+
+            //if (alex == simon)
+            //{
+            //    Console.WriteLine(alex.FirstName + " and " + simon.FirstName + " are indentical.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(alex.FirstName + " and " + simon.FirstName + " are NOT indentical.");
+            //}
+
+            //if (simon == simonClone)
+            //{
+            //    Console.WriteLine(simon.FirstName + " and " + simonClone.FirstName + " are indentical.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(simon.FirstName + " and " + simonClone.FirstName + " are NOT indentical.");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
